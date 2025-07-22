d5bebab7b901ef4802ebba5e3392f8d3345195e6
Song Do Hyeon <fsxsong@gmail.com>
2025-07-22 13:53:40 +0900

    first

---
+++ src/main/java/com/example/megacity_back/MegacityBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.megacity_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegacityBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegacityBackApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/megacity_back/repository/RepDataReFcstLfdDaRepository.java
@@ -0,0 +1,4 @@
+package com.example.megacity_back.repository;
+
+public interface RepDataReFcstLfdDaRepository {
+}
