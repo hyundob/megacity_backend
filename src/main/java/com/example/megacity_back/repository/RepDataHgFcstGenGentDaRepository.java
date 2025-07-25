0a610d0e80809f6fb13fa8e4012bcc8273495fc9
Song Do Hyeon <fsxsong@gmail.com>
2025-07-25 14:52:30 +0900

    2025-07-25 add

---
+++ src/main/java/com/example/megacity_back/repository/RepDataHgFcstGenGentDaRepository.java
@@ -0,0 +1,7 @@
+package com.example.megacity_back.repository;
+
+import com.example.megacity_back.entity.*;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepDataHgFcstGenGentDaRepository extends CrudRepository<RepDataHgFcstGenGentDa, FcstGenGentDaId> {
+}
---
+++ src/main/java/com/example/megacity_back/repository/RepDataReKpxJejuSukubMRepository.java
@@ -0,0 +1,7 @@
+package com.example.megacity_back.repository;
+
+import com.example.megacity_back.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepDataReKpxJejuSukubMRepository extends JpaRepository<RepDataReKpxJejuSukubM, Long> {
+}
