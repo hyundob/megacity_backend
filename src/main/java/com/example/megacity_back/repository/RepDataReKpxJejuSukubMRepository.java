f46ef055aea5ead400ec8ed76eac57190bf420ad
Song Do Hyeon <fsxsong@gmail.com>
2025-09-16 11:45:26 +0900

    2025-09-16 repository refactoring

--- src/main/java/com/example/megacity_back/repository/RepDataReKpxJejuSukubMRepository.java
+++ src/main/java/com/example/megacity_back/repository/RepDataReKpxJejuSukubMRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface RepDataReKpxJejuSukubMRepository extends JpaRepository<RepDataReKpxJejuSukubM, Long> {
+public interface RepDataReKpxJejuSukubMRepository extends JpaRepository<RepDataReKpxJejuSukubM, String> {
     Optional<RepDataReKpxJejuSukubM> findTopByOrderByTmDesc();
     List<RepDataReKpxJejuSukubM> findByTmStartingWith(String tm);
 }
